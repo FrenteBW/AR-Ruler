9489e6c58867f39469e6e6bf7c0b7ad589f99215
An Byungwook <abw2619@icloud.com>
2022-05-06 17:10:34 +0900

    First commit

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,20 +13,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,13 +36,90 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    
     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+            
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView){
+            let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResult.first{
+                addDot(at: hitResult)
+            }
+        }
+        
+        func addDot(at hitResult : ARHitTestResult){
+            let dotGeometry = SCNSphere(radius: 0.005)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.red
+            
+            dotGeometry.materials = [material]
+            
+            let dotNode = SCNNode(geometry: dotGeometry)
+            
+            dotNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y, z: hitResult.worldTransform.columns.3.z)
+            
+            sceneView.scene.rootNode.addChildNode(dotNode)
+            
+            dotNodes.append(dotNode)
+            
+            if dotNodes.count >= 2 {
+                calculate()
+            }
+        }
+        
+        func calculate (){
+            let start = dotNodes[0]
+            let end = dotNodes[1]
+            
+            print(start.position)
+            print(end.position)
+            
+            let distance = sqrt(
+                pow(end.position.x - start.position.x, 2) +
+                pow(end.position.y - start.position.y,2) +
+                pow(end.position.z - start.position.z, 2))
+            //distance = √ ((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+            
+            updateText(text : "\(abs(distance))", atPosition: end.position)
+            
+            
+        }
+        
+        func updateText(text: String, atPosition position: SCNVector3){
+            
+            textNode.removeFromParentNode()
+            
+            let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+            
+            textGeometry.firstMaterial?.diffuse.contents = UIColor.white
+            
+            textNode = SCNNode(geometry: textGeometry)
+            
+            textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+            
+            textNode.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+            
+            sceneView.scene.rootNode.addChildNode(textNode)
+            
+        }
+        
+    }
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -55,7 +129,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
      
         return node
     }
-*/
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -71,4 +145,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+     */
 }
